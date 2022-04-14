fac7162444d0c453c9bcda28a548da2f9a86f624
João Henrique <joao.henrique-1998@hotmail.com>
2022-04-14 20:08:30 -0300

    add theme dark

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@mui/material';
 import { Routes , Route} from 'react-router-dom';
+import { useAppThemeContext } from '../context';
 
 export const AppRoutes = () => {
+    const { toggleTheme } = useAppThemeContext();
     return (
          <Routes>
-         <Route path="/" element={<Button variant='contained' color='primary'>Teste</Button>}/>
+         <Route path="/" element={<Button variant='contained' color='primary' onClick={ toggleTheme }>Teste</Button>}/>
          </Routes>
         );
 }
